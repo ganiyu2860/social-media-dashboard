bfb7d2ffe57ade22011136136f8bf4db04bb9285
Yitian Luo <yitianluo@YitiandeMacBook-Pro.local>
2020-06-07 18:48:00 +0800

    try button click

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 //jshint esversion:6
 alert("hello");
-var root = $(":root" );
+var root = $(":root");
+var darkMode = true;
 
-$(document).ready(function(){
-    $("div.clickable").on('click', function () {
-        alert('Click');
-    });
+$(document).ready(function() {
+  $("button").on('click', function() {
+    alert('Click');
+  });
 });
 
-$(document).on("change", "input[name='mode-button']", function () {
-  alert("change");
-  var checked = $("input").prop("checked");
+$(document).on("click", "button[name='mode-button']", function() {
+  // var checked = $("input").prop("checked");
   //light theme
-  if(this.checked === true){
-    root.css("--bg-color","white");
+  if (darkMode === true) {
+    alert("darkMode");
+    root.css("--bg-color", "white");
     root.css('--top-color', "hsl(225, 100%, 98%)");
-    root.css("--head-color","hsl(230, 17%, 14%)");
-    root.css("--text-color","hsl(228, 12%, 44%)");
-    root.css("--card-bg","hsl(227, 47%, 96%)");
-  }
-  // dark theme
-  if(this.checked === false){
-    root.css("--bg-color","hsl(230, 17%, 14%)");
+    root.css("--head-color", "hsl(230, 17%, 14%)");
+    root.css("--text-color", "hsl(228, 12%, 44%)");
+    root.css("--card-bg", "hsl(227, 47%, 96%)");
+    darkMode = false;
+  } else {
+    // dark theme
+    alert("lightMode");
+    root.css("--bg-color", "hsl(230, 17%, 14%)");
     root.css("--top-color", "hsl(232, 19%, 15%)");
-    root.css("--head-color","white");
-    root.css("--text-color","hsl(228, 34%, 66%)");
-    root.css("--card-bg","hsl(228, 28%, 20%)");
+    root.css("--head-color", "white");
+    root.css("--text-color", "hsl(228, 34%, 66%)");
+    root.css("--card-bg", "hsl(228, 28%, 20%)");
+    darkMode = true;
   }
 });
