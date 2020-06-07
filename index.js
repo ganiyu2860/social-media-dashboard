704eeaa866a426c08ce506093fa97a23c7d4a65f
Yitian Luo <yitianluo@YitiandeMacBook-Pro.local>
2020-06-07 20:08:55 +0800

    test jquery and dom together hello2

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//jshint esversion:6
 alert("hello1");
 var root = $(":root");
 var topbg = $(".top-container");
@@ -9,6 +8,7 @@ root.css("--bg-color", "white");
 topbg.css("background-color","yellow");
 
 document.getElementsByClassName("top-container")[0].style.backgroundColor = "red";
+alert("hello2");
 // root.css('--top-color', "hsl(225, 100%, 98%)");
 // root.css("--head-color", "hsl(230, 17%, 14%)");
 // root.css("--text-color", "hsl(228, 12%, 44%)");
