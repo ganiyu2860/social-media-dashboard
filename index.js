3427f427916f36d5765dc8360a544bf2fd5cb38c
Yitian Luo <yitianluo@YitiandeMacBook-Pro.local>
2020-06-07 20:54:51 +0800

    finish mobile bug

--- index.js
+++ index.js
@@ -9,11 +9,10 @@ var root = $(":root");
 
 // $(document).on("click", "button[name='mode-button']", function() {
 $(".switch").change(function(){
-  alert("hello");
+
   var checked = $("input").prop("checked");
   //light theme
   if (checked === true) {
-    alert("darkMode");
     root.css("--bg-color", "white");
     root.css('--top-color', "hsl(225, 100%, 98%)");
     root.css("--head-color", "hsl(230, 17%, 14%)");
@@ -22,7 +21,6 @@ $(".switch").change(function(){
     darkMode = false;
   } else {
     // dark theme
-    alert("lightMode");
     root.css("--bg-color", "hsl(230, 17%, 14%)");
     root.css("--top-color", "hsl(232, 19%, 15%)");
     root.css("--head-color", "white");
